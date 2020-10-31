ef1c8b0deca8400f40287c35e6a3478aa3447aa8
Bizet <liubicai@outlook.com>
2020-10-31 14:37:16 +0800

    Add project files.

---
+++ ShowPlayerName/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ShowPlayerName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.raingBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(70, 12);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(130, 28);
+            this.nameBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "球员名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "等级";
+            // 
+            // raingBox
+            // 
+            this.raingBox.Location = new System.Drawing.Point(266, 14);
+            this.raingBox.Name = "raingBox";
+            this.raingBox.Size = new System.Drawing.Size(55, 28);
+            this.raingBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(6, 52);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(691, 354);
+            this.listView.TabIndex = 7;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView_ItemSelectionChanged);
+            this.listView.Click += new System.EventHandler(this.listView_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "球员名";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "等级";
+            this.columnHeader3.Width = 40;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "最低价";
+            this.columnHeader4.Width = 70;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "最高价";
+            this.columnHeader5.Width = 70;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "市一价";
+            this.columnHeader6.Width = 70;
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(536, 12);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(94, 28);
+            this.idBox.TabIndex = 0;
+            this.idBox.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 406);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.raingBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.idBox);
+            this.Name = "Form1";
+            this.Text = "查球员2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox raingBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ShowPlayerName/Form1.cs
@@ -0,0 +1,157 @@
+﻿using AppNerve.Expands.StringExpand;
+using AppNerve.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowPlayerName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        playerInfos playersInfo = JsonConvert.DeserializeObject<playerInfos>(
+                    Encoding.UTF8.GetString(Resource.playerInfo2));
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {           
+           // nameBox.Text=playersInfo.GetName(int.Parse(idBox.Text));
+        }
+        public class playerInfos
+        {
+            public playerInfo[] LegendsPlayers { get; set; }
+            public playerInfo[] Players { get; set; }
+
+            public string GetName(int id)
+            {
+                foreach (var player in LegendsPlayers)
+                {
+                    if (player.id == id)
+                        return player.l;
+                }
+
+                foreach (var player in Players)
+                {
+                    if (player.id == id)
+                        return player.l;
+                }
+
+                return "";
+            }
+
+            public List<playerInfo> GetIdByNameAndRating(string name,int rating)
+            {
+                List<playerInfo> ls = new List<playerInfo>();
+                foreach(var player in LegendsPlayers)
+                {
+                    if(player.r==rating||rating==0)
+                    {
+                        if (player.f.Contains(name) || player.l.Contains(name))
+                        {
+                            ls.Add(player);
+                        }
+                    }
+                    
+                }
+                foreach (var player in Players)
+                {
+                    if (player.r == rating||rating==0)
+                    {
+                        if (player.f.Contains(name) || player.l.Contains(name))
+                        {
+                            ls.Add(player);
+                        }
+                    }
+
+                }
+                return ls;
+            }
+
+        }
+        public class playerInfo
+        {
+            public string f { get; set; }
+            public int id { get; set; }
+            public string l { get; set; }
+            public int r { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rating = 0;
+            int.TryParse(raingBox.Text, out rating);
+            List<playerInfo> players = playersInfo.GetIdByNameAndRating(nameBox.Text, rating);
+            listView.Items.Clear();
+            foreach(var player in players)
+            {
+                HttpClient client = new HttpClient();
+                Response data=client.GetData("https://www.futbin.com/20/playerPrices?player=" + player.id);
+                FutbinPrice futbinPrice = JsonConvert.DeserializeObject<FutbinPrice>(data.Html.Match("\"ps\":({.*?})"));
+                //添加行
+                var item = new ListViewItem();
+                //item.ImageIndex = 1;
+                item.Text = player.id.ToString(); 
+                item.SubItems.Add(player.f+" "+player.l);
+                item.SubItems.Add(player.r.ToString());
+                item.SubItems.Add(futbinPrice.MinPrice); 
+                item.SubItems.Add(futbinPrice.MaxPrice);
+                item.SubItems.Add(futbinPrice.LCPrice);
+                listView.BeginUpdate();
+                listView.Items.Add(item);
+                listView.Items[listView.Items.Count - 1].EnsureVisible();//滚动到最后
+                listView.EndUpdate();
+               
+
+            }
+            idBox.Text = listView.Items[0].Text;
+
+           
+            
+            
+           
+        }
+
+        private void listView_Click(object sender, EventArgs e)
+        {
+            idBox.Text = listView.SelectedItems[0].Text;
+        }
+
+        private void listView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class FutbinPrice
+    {
+        public string LCPrice { get; set; }
+
+        public string LCPrice2 { get; set; }
+
+        public string LCPrice3 { get; set; }
+
+        public string LCPrice4 { get; set; }
+
+        public string LCPrice5 { get; set; }
+
+        public string updated { get; set; }
+
+        public string MinPrice { get; set; }
+
+        public string MaxPrice { get; set; }
+
+        public string PRP { get; set; }
+    }
+}
